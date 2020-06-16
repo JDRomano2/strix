3b581f95e36ee5ba9a128f1c276ce9c5050dd185
Joseph D. Romano <jdromano2@gmail.com>
2020-06-16 19:01:01 -0400

    Switch from .h to .hpp (for various reasons)

--- standalone/src/main.cpp
+++ standalone/src/main.cpp
@@ -2,7 +2,18 @@
 
 #include <strix/strix.h>
 
+#include <pugixml.h>
+
 int main() {
   strix::Strix st();
   std::cout << "Why hello there" << std::endl;
+
+  // Parse the Pizza ontology
+  char* pizza_fname = "data/pizza.owl";
+  pugi::xml_document pizza_doc;
+  pugi::xml_parse_result pizza = pizza_doc.load_file(pizza_fname);
+
+  std::cout << "Load result: " << pizza.description() << ", mesh name: " << 
+  pizza_doc.child("mesh").attribute("name").value() << std::endl;
+
 }
